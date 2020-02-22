94596d28679ff0d46ffd6d7cfc92871acca7953f
Katherine Byers <55606873+pingwoh@users.noreply.github.com>
2020-02-22 00:40:16 -0600

    audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds; //create an array of sounds to play
+
+    public static AudioManager Instance; //create an instance of the audio manager
+
+    private void Awake()
+    {
+        if (Instance == null) //if there is none
+            Instance = this; //set it equal to 'this'; note: hard to explain
+        else
+        {
+            Destroy((gameObject)); //destroy the object
+            return; //exit function
+        }
+        DontDestroyOnLoad(gameObject); //keep the manager from scene to scene
+
+        foreach (Sound s in sounds) //populate the array w/ sound clips
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void PlayAudio(string SoundName)
+    {
+        //find the sound specified and play it back
+        Sound s = Array.Find(sounds, sound => sound.name == SoundName);
+        if (s == null) return;
+        s.source.Play();
+    }
+
+    public void Stop(string SoundName)
+    {
+        //find the sound specified and stop it
+        Sound s = Array.Find(sounds, sound => sound.name == SoundName);
+        s.source.Stop();
+    }
+
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+//so that you can put everything in in the editor!
+// names, the clip u want, the volume, and if it loops or not!
+public class Sound
+{
+
+    public string name;
+    public AudioClip clip;
+    [Range(0f, 1f)] public float volume;
+    public bool loop;
+    [HideInInspector] public AudioSource source;
+}
