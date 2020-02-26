cf23fe134b07e13bc0da404c6200e94f87448ac5
ASUMSTAD <asumstad@utexas.edu>
2020-02-26 02:48:28 -0600

    Setting up endings correctly

---
+++ Assets/Scripts/NPC Activations/EndingEvents.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingEvents : MonoBehaviour
+{
+   public void BloodlustEnd()
+    {
+
+    }
+
+    public void NeutralEnd()
+    {
+
+    }
+
+    public void VampKillEnd()
+    {
+
+    }
+}
