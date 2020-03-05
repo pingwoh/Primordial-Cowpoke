b1d1b54e017d27f07766adf170e4483f37186cf4
westonwright <57042424+westonwright@users.noreply.github.com>
2020-03-04 23:51:50 -0600

    Combining Builds

--- Assets/Scripts/NPC Activations/EndingEvents.cs
+++ Assets/Scripts/NPC Activations/EndingEvents.cs
@@ -8,19 +8,30 @@ public class EndingEvents : MonoBehaviour
     public GameObject BloodlustAnimator;
     public GameObject NeutralAnimator;
     public GameObject VampKillAnimator;
+    public GameObject fadeAnimator;
+    public GameObject BackgroundMusic;
 
    public void BloodlustEnd()
     {
+        fadeAnimator.gameObject.SetActive(true);
         BloodlustAnimator.gameObject.SetActive(true);
+        Destroy(BackgroundMusic);
+        Instantiate(Resources.Load("VampireEndMusic"), transform.position, Quaternion.identity);
     }
 
     public void NeutralEnd()
     {
+        fadeAnimator.gameObject.SetActive(true);
         NeutralAnimator.gameObject.SetActive(true);
+        Destroy(BackgroundMusic);
+        Instantiate(Resources.Load("NeutralEndMusic"), transform.position, Quaternion.identity);
     }
 
     public void VampKillEnd()
     {
+        fadeAnimator.gameObject.SetActive(true);
         VampKillAnimator.gameObject.SetActive(true);
+        Destroy(BackgroundMusic);
+        Instantiate(Resources.Load("VillageEndMusic"), transform.position, Quaternion.identity);
     }
 }
